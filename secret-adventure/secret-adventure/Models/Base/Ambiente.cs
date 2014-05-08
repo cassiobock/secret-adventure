ef0b342149b3969caaa2b7b1566a1a581d823575
Cassio <cassio.bock@gmail.com>
2014-05-08 01:28:23 -0300

    Início da modelagem do ambiente

---
+++ secret-adventure/secret-adventure/Models/Base/Ambiente.cs
@@ -0,0 +1,29 @@
+﻿using Dengue.Models.Outra;
+using secret_adventure.Models.Manager;
+using secret_adventure.Models.Other;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace secret_adventure.Models.Base
+{
+    public class Ambiente
+    {
+        public List<Entidade> Entidades;
+        public int Linhas;
+        public int Colunas;
+        public List<Entidade> PersonagensQueSeraoAdicionados = new List<Entidade>();
+
+        public Ambiente(int linhas, int colunas, int qtdMosquitos, int qtdPessoas, int qtdAgentes)
+        {
+            this.Linhas = linhas;
+            this.Colunas = colunas;
+            List<Entidade> entidadesSendoCriadas = new List<Entidade>();
+            new AmbienteManager(this).GeraEntidades(TipoClasse.Agente, qtdAgentes, entidadesSendoCriadas);
+            new AmbienteManager(this).GeraEntidades(TipoClasse.Mosquito, qtdMosquitos, entidadesSendoCriadas);
+            new AmbienteManager(this).GeraEntidades(TipoClasse.Pessoa, qtdPessoas, entidadesSendoCriadas);
+            this.Entidades = Util.Shuffle<Entidade>(entidadesSendoCriadas);
+        }
+    }
+}
---
+++ secret-adventure/secret-adventure/Models/Manager/AmbienteManager.cs
@@ -0,0 +1,72 @@
+﻿using Dengue.Models.Outra;
+using secret_adventure.Models.Base;
+using secret_adventure.Models.Factory;
+using secret_adventure.Models.Other;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+
+namespace secret_adventure.Models.Manager
+{
+    public class AmbienteManager
+    {
+        private Ambiente Ambiente;
+
+        /// <summary>
+        /// Cria um gerenciador de ambientes
+        /// </summary>
+        /// <param name="ambiente"></param>
+        public AmbienteManager(Ambiente ambiente)
+        {
+            this.Ambiente = ambiente;
+        }
+
+        /// <summary>
+        /// Retorna uma matriz com as entidades 
+        /// </summary>
+        /// <returns></returns>
+        public Entidade[,] GetMatriz()
+        {
+            Entidade[,] matriz = new Entidade[this.Ambiente.Linhas, this.Ambiente.Colunas];
+            foreach (var entidade in this.Ambiente.Entidades)
+            {
+                if (matriz[entidade.Posicao.X, entidade.Posicao.Y] != null)
+                {
+                    matriz[entidade.Posicao.X, entidade.Posicao.Y] = entidade;
+                }
+                else
+                {
+                    throw new Exception("Existem entidades com posições duplicadas.");
+                }
+            }
+            return matriz;
+        }
+
+        internal void GeraEntidades(TipoClasse tipoClasse, int qtdPersonagens, List<Entidade> entidadesSendoCriadas)
+        {
+            for (int i = 1; i <= qtdPersonagens; i++)
+            {
+                Point posicao = Util.GeraPosicao(this.Ambiente.Linhas, this.Ambiente.Colunas);
+                // Verifica se já existe alguém na posição selecionada
+                int entidades = this.Ambiente.Entidades.Count;
+                while (this.Ambiente.Entidades.Count(m => m.Posicao == posicao) == 0)
+                {
+                    // Gera uma nova posição
+                    if (entidades != 0)
+                    {
+                        posicao = Util.GeraPosicao(this.Ambiente.Linhas, this.Ambiente.Colunas);
+                        entidades--;
+                    }
+                    else
+                    {
+                        throw new Exception("Sem espaços disponíveis.");
+                    }
+                }
+                //Adiciona na lista
+                entidadesSendoCriadas.Add(EntidadeFactory.CriaTipoPersonagem(tipoClasse, posicao));
+            }
+        }
+    }
+}
