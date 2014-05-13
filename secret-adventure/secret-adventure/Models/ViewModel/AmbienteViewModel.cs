30aeea5685a71ee55b5bb4660cbb0db7c6d4999f
Cassio <cassio.bock@gmail.com>
2014-05-13 02:46:16 -0300

    Estilos adicionados

--- secret-adventure/secret-adventure/Models/ViewModel/AmbienteViewModel.cs
+++ secret-adventure/secret-adventure/Models/ViewModel/AmbienteViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,15 @@ namespace secret_adventure.Models.ViewModel
 {
     public class AmbienteViewModel
     {
+        [DisplayName("Linhas")]
         public int Linhas { get; set; }
+        [DisplayName("Colunas")]
         public int Colunas { get; set; }
+        [DisplayName("Número de Mosquitos")]
         public int NumeroMosquitos { get; set; }
+        [DisplayName("Número de pessoas")]
         public int NumeroPessoas { get; set; }
+        [DisplayName("Número de Agente")]
         public int NumeroAgentes { get; set; }
     }
 }
