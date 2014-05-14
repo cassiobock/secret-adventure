cc2ac97d95afa586f8430e134b27e284abe362d0
Cassio <cassio.bock@gmail.com>
2014-05-14 13:57:44 -0300

    Typo e remoção de código desnecessário

--- secret-adventure/secret-adventure/Models/ViewModel/AmbienteViewModel.cs
+++ secret-adventure/secret-adventure/Models/ViewModel/AmbienteViewModel.cs
@@ -16,13 +16,13 @@ namespace secret_adventure.Models.ViewModel
         [Required(ErrorMessage = "Você deve preencher o número de colunas do ambiente!")]
         public int Colunas { get; set; }
         [DisplayName("Número de Mosquitos")]
-        [Required(ErrorMessage = "Você deve preencher o número de mosquitos!")]
+        [Required(ErrorMessage = "Você deve preencher o número de Mosquitos!")]
         public int NumeroMosquitos { get; set; }
         [DisplayName("Número de pessoas")]
-        [Required(ErrorMessage = "Você deve preencher o número de pessoas!")]
+        [Required(ErrorMessage = "Você deve preencher o número de Pessoas!")]
         public int NumeroPessoas { get; set; }
         [DisplayName("Número de Agente")]
-        [Required(ErrorMessage = "Você deve preencher o número de agente!")]
+        [Required(ErrorMessage = "Você deve preencher o número de Agentes!")]
         public int NumeroAgentes { get; set; }
     }
 }
